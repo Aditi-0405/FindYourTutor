e0f71dccb6a292e2764f97d05f7e0b07b80eac11
Aditi-0405 <somyaaditi02@gmail.com>
2024-06-08 10:04:54 +0530

    added student dashboard and common dashboard styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import RegisterTutor from './Tutors/TutorPages/Register';
 import LoginStudent from './Students/StudentPages/Login';
 import LoginTutor from './Tutors/TutorPages/Login';
 import TutorDashboard from './Tutors/TutorPages/TutorDashboard';
+import StudentDashboard from './Students/StudentPages/StudentDashboard';
 import TutorProfile from './Tutors/TutorPages/TutorProfile';
 import StudentProfile from './Students/StudentPages/StudentProfile';
 const App = () => {
@@ -23,6 +24,7 @@ const App = () => {
           <Route path="/login-student" element={<LoginStudent setIsLoggedIn={setIsLoggedIn}/>} />
           <Route path="/login-tutor" element={<LoginTutor setIsLoggedIn={setIsLoggedIn}/>} />
           <Route path="/tutor-dashboard" element={<TutorDashboard/>} />
+          <Route path="/student-dashboard" element={<StudentDashboard/>} />
           <Route path="/tutor-profile" element={<TutorProfile/>} />
           <Route path="/student-profile" element={<StudentProfile/>} />
         </Routes>
